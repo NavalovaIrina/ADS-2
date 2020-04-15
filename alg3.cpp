54ca962bca0a9e4d8bc8beff3ff789c2198c9bed
NavalovaIrina <62655840+NavalovaIrina@users.noreply.github.com>
2020-04-15 17:37:04 +0300

    Update alg3.cpp

--- alg3.cpp
+++ alg3.cpp
@@ -1,28 +1,9 @@
-void sort(int* arr, int size)
-{
-int temp = 0;
-	for (int i = 0; i < size - 1; i++)
-	{
-		for (int j = 0; j < size - i - 1; j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-
-		}
-
-	}
-}
-
 int countPairs1(int* arr, int len, int value)
 {
 	int k = 0;
 	for (int i = 0; i < len - 1; i++)
 	{
-		for (int j = i + 1; j < len && (i != j); j++)
+		for (int j = i + 1; j < len; j++)
 		{
 			if (arr[i] + arr[j] == value)
 			{
